54b94cd60a68c017b79c9d30e743cb02e39e06cc
ODET Alexandre <madddogg39@gmail.com>
2020-01-02 13:05:33 +0100

    Initial Commit

---
+++ Schibsted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schibsted
+//
+//  Created by Odet Alexandre on 02/01/2020.
+//  Copyright © 2020 Odet Alexandre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
